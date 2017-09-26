f88c56aefa2bd9920e7936737a8b0d52e2b2e5b9
Karan Puniani <puniani.karan@gmail.com>
2017-09-25 17:53:22 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express =require('express');
+const hbs= require('hbs');
+const fs= require('fs');
+var app=express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+
+
+hbs.registerHelper('getCurrentYear',()=>{
+return new Date().getFullYear()
+});
+
+hbs.registerHelper('doCapitals',(text)=>{
+return text.toUpperCase();
+});
+
+app.use((req,res,next)=>{
+  var time =new Date().toString();
+  var log =`${time} : ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log',log +'\n')
+  next();
+});
+app.use((req,res,next)=>{
+res.render('maintaince.hbs',{
+PageTitle:"Something went wrong",
+WelcomeMessage:"We will come back soon"
+});
+});
+app.use(express.static(__dirname+'/public'))
+app.get('/',(req,res)=>{
+  res.render('home.hbs',{
+  PageTitle:'You are welcome',
+  WelcomeMessage:"welcome to our website"
+  });
+});
+app.get('/about',(req,res)=>{
+res.render('about.hbs',{
+PageTitle:'About Page',
+WelcomeMessage:"welcome to about page website"
+});
+});
+
+
+app.get('/bad',(req,res)=>{
+ res.send({
+errorMessage:'This is a bad request'
+ });
+});
+app.listen(3000,()=>{
+
+  console.log('Server is up on 3000');
+});
